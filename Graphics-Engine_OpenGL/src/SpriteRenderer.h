64475510ccff5d5eb7dfacdae20d7b55fde8f2e7
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-11-09 13:42:38 -0300

    Added Sprite Renderer component

---
+++ Graphics-Engine_OpenGL/src/SpriteRenderer.h
@@ -0,0 +1,19 @@
+#ifndef SPRITERENDERER_H
+#define SPRITERENDERER_H
+
+#include "Script.h"
+
+class SpriteRenderer : public Script {
+	float vertex[32] = {
+		 0.5f,  0.5f, 0.0f,   0.0f, 1.0f, 1.0f,
+		 0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 0.0f,
+		-0.5f,  0.5f, 0.0f,   0.0f, 1.0f, 1.0f
+	};
+	unsigned int index[6] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+};
+
+#endif // !SPRITERENDERER_H
